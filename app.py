d86c28285e047c9c4ad4ffe99f54d60dc6e2989b
synthpop123 <2020393267@qq.com>
2024-08-23 21:01:37 +0800

    feat: Add healthcheck function

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+
+    Returns:
+        A JSON response with the health status
+    """
+    return HealthCheck(status="OK")
+
 
 @app.get("/")
 def index():
